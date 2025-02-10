e1483d56a060c9ac8ed463098bc1c3c9eea3c44d
KAUE DOS SANTOS ANDRADE <082220027@faculdade.cefsa.edu.br>
2025-02-10 16:56:34 -0300

    Análise de dados primária concluída

--- Projeto estágio.py
+++ Projeto estágio.py
@@ -6,25 +6,9 @@ file_path = "C:/Users/kauea/OneDrive/Área de Trabalho/ERROR 404/Faculdade/Está
 df = pd.read_parquet(file_path)
 
 # Visualize as primeiras linhas
-#print(df.head())
+print(df.head())
 
 # Verificar colunas e tipos
-#print(df.info())
+print(df.info())
 
-# Verificar estatísticas básicas
-#print(df.describe())
 
-# Filtrar apenas veículos de um certo tipo
-#filtered_df = df[df["Denominação"] == "O-500U 1826/59 EURO5"]
-#print(filtered_df)
-
-# Criar uma coluna calculada (exemplo)
-#df["DTLCO"] = pd.to_datetime(df["DTLCO"]).dt.year
-#print(df)
-
-#df["Denominação"].value_counts().plot(kind="line")
-#plt.show()
-
-# Exemplo de combinar bases
-df2 = pd.read_parquet("C:/Users/kauea/OneDrive/Área de Trabalho/ERROR 404/Faculdade/Estágio/Case/Case/cod_pais.parquet")
-merged_df = pd.merge(df, df2, on="CodPais")
