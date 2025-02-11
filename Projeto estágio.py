b660fb1c19a0cb0a237c9efc13347361b079cc14
KAUE DOS SANTOS ANDRADE <082220027@faculdade.cefsa.edu.br>
2025-02-11 01:52:30 -0300

    Primeiro DashBoard

--- Projeto estágio.py
+++ Projeto estágio.py
@@ -9,10 +9,6 @@ Tabela1 = pd.read_parquet(file_path)
 # Carrega o arquivo Parquet com os dados dos países
 Tabela2 = pd.read_parquet("C:/Users/kauea/OneDrive/Área de Trabalho/ERROR 404/Faculdade/Projeto-est-gio-Mercedes/Case/Case/cod_pais.parquet")
 
-#Inner Join dos dois Parquets
-TabelasConjuntas = pd.merge(Tabela1, Tabela2, on=['Codigo_Pais'], how='inner')
-print(TabelasConjuntas.head())
-
 # Renomear colunas para facilitar integração
 Tabela1.rename(columns={'CodPais': 'Codigo_Pais', 'Denominação': 'Tipo_Veiculo', 
                     'varNED': 'Subtipo_Veiculo', 'NP': 'Numero_Registro', 
@@ -25,35 +21,19 @@ date_columns = ['Data_Producao', 'Data_Liberacao_Parcial', 'Data_Liberacao_Compl
 for col in date_columns:
     Tabela1[col] = pd.to_datetime(Tabela1[col], format='%d%m%y', errors='coerce')
 
+#Inner Join dos dois Parquets
+TabelasConjuntas = pd.merge(Tabela1, Tabela2, on=['Codigo_Pais'], how='inner')
+print(TabelasConjuntas.head())    
+
 # Remover duplicatas com base na chave primária (Numero_Registro)
 Tabela1.drop_duplicates(subset='Numero_Registro', inplace=True)
 
 # Preencher valores ausentes ou tratá-los conforme necessário
 Tabela1.fillna({'Tipo_Veiculo': 'Desconhecido', 'Subtipo_Veiculo': 'Indefinido'}, inplace=True)
 
-#Verificar colunas e tipos
-print(Tabela1.info())
-print("")
-print(Tabela2.info())
-
-#Verificar estatísticas básicas
-print(Tabela1.describe())
-
-#Filtrar apenas veículos de um certo tipo
-filtered_df = Tabela1[Tabela1["Tipo_Veiculo"] == "O-500U 1826/59 EURO5"]
-print(filtered_df.head())
-
-#Cria uma tabela genérica
-Tabela1["Data_Liberacao_Parcial"].value_counts().plot(kind="bar")
-plt.show()
-
-# Agrupar e contar veículos por país
-veiculos_por_pais = TabelasConjuntas.groupby('Codigo_Pais')['Numero_Registro'].count()
-print(veiculos_por_pais)
-
 # Salvar como Excel para trabalhar no PowerBI
-Tabela1.to_excel('TabelaVeiculos.xlsx', index=False, engine='openpyxl')
-Tabela2.to_excel('TabelaPaises.xlsx', index=False, engine='openpyxl')
-TabelasConjuntas.to_excel('TabelaConjunta.xlsx', index=False, engine='openpyxl')
+#Tabela1.to_excel('TabelaVeiculos.xlsx', index=False, engine='openpyxl')
+#Tabela2.to_excel('TabelaPaises.xlsx', index=False, engine='openpyxl')
+#TabelasConjuntas.to_excel('TabelaConjunta.xlsx', index=False, engine='openpyxl')
 
 print("Arquivo Excel salvo com sucesso!")
